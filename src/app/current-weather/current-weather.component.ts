458555d44348b34f93a63cd8201a7c0e91c9a2c9
cloudobserver <cloudobserver@gmail.com>
2021-01-08 13:56:48 -0900

    Add binding and interface

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from "../icurrent-weather";
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit {
+
+  current: ICurrentWeather
+  constructor() {
+    this.current = {
+      city: 'Redmond',
+      country: 'US',
+      date: new Date(),
+      image: '',
+      temperature: 59,
+      description: 'rainy'
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
